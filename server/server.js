c7906d28a9be8e6a10a535d838a747bdf46c4053
Rutilio Iraheta <rutilio.irahetarivera@telus.com>
2018-11-23 00:19:37 -0600

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID}=require('mongodb');
 
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -23,6 +24,30 @@ app.post('/todos',(req, res)=>{
     });
 });
 
+app.get('/todos',(req, res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos})
+    }, (e)=>{
+        res.status(400).send(e);
+    });
+});
+
+
+app.get('/todos/:id', (req, res )=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.sendStatus(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if (!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+    //res.send(req.params);
+});
 
 app.listen(3000, ()=>{
     console.log('Started on port 3000');
